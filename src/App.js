6dbf091f1a516b9aed0a5f0fdd1e60f98c4b6ecb
Kristina Chausheva <k.chausheva@gmail.com>
2020-07-14 13:54:34 +0300

    feat: implementing ogiramis container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './components/header';
+import Aside from './components/aside';
+import Origamis from './components/origamis';
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
+      <div>
+        <Aside />
+        <Origamis />
+      </div>
     </div>
   );
 };
