939bdac2674e5fe054065395953d2fa37fd5853f
Kristina Chausheva <k.chausheva@gmail.com>
2020-07-14 14:37:00 +0300

    footer implementation

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import Link from '../link';
 import styles from './index.module.css';
-import logo from '../../images/white-origami-bird.png';
 
-const Header = () => {
+const Footer = () => {
   return (
-    <header className={styles.navigation}>
-      <img className={styles.logo} src={logo} />
+    <footer className={styles.footer}>
       <Link href='#' title='Going to 1' type='header' />
       <Link href='#' title='Going to 2' type='header' />
       <Link href='#' title='Going to 3' type='header' />
       <Link href='#' title='Going to 4' type='header' />
       <Link href='#' title='Going to 5' type='header' />
       <Link href='#' title='Going to 6' type='header' />
-    </header>
+      <p>Software University &copy;2020</p>
+    </footer>
   );
 };
 
-export default Header;
+export default Footer;
